dbb91a74a2ba2b249215b7cde1dc1bb4ca660803
panplemousse <49610933+panplemousse@users.noreply.github.com>
2025-10-04 18:40:41 +0200

    Change source language from 'cs' to 'cz'

--- assignment1.sh
+++ assignment1.sh
@@ -15,7 +15,7 @@ export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
 
 # PREPARE DATA
 python preprocess.py \
-    --source-lang cs \
+    --source-lang cz \
     --target-lang en \
     --raw-data ~/shares/cz-en/data/raw \
     --dest-dir ./cz-en/data/prepared \
@@ -25,14 +25,14 @@ python preprocess.py \
     --valid-prefix valid \
     --src-vocab-size 8000 \
     --tgt-vocab-size 8000 \
-    --src-model ./cz-en/tokenizers/cs-bpe-8000.model \
+    --src-model ./cz-en/tokenizers/cz-bpe-8000.model \
     --tgt-model ./cz-en/tokenizers/en-bpe-8000.model
 
 # TRAIN
 python train.py \
     --cuda \
     --data cz-en/data/prepared/ \
-    --src-tokenizer cz-en/tokenizers/cs-bpe-8000.model \
+    --src-tokenizer cz-en/tokenizers/cz-bpe-8000.model \
     --tgt-tokenizer cz-en/tokenizers/en-bpe-8000.model \
     --source-lang cs \
     --target-lang en \
@@ -56,7 +56,7 @@ python train.py \
 python translate.py \
     --cuda \
     --input cz-en/data/raw/test.cz \
-    --src-tokenizer cz-en/tokenizers/cs-bpe-8000.model \
+    --src-tokenizer cz-en/tokenizers/cz-bpe-8000.model \
     --tgt-tokenizer cz-en/tokenizers/en-bpe-8000.model \
     --checkpoint-path cz-en/checkpoints/checkpoint_best.pt \
     --output cz-en/output.txt \
