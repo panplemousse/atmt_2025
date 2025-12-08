930adc916433cdb80156a5fe6a46bd4171e11e77
panplemousse <49610933+panplemousse@users.noreply.github.com>
2025-12-08 20:04:33 +0000

    update gpu and mamba module in sh script

--- assignment5_3.sh
+++ assignment5_3.sh
@@ -7,8 +7,8 @@
 #SBATCH --gpus=1
 #SBATCH --output=beam_and_optimize_assignment5.out
 
-module load gpu
-module load mamba
+module load a100
+module load miniforge3
 source activate atmt
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
 
